9c3ffff1c0be16b425c5efd45fe512036adb8162
Ricardo Enciso <rreyes@glwinba.com>
2022-09-27 12:40:12 -0500

    Menú de agregar tarea completado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 //import './App.css';
 import Header from './components/Header.jsx';
-import PendingTasks from './components/PendingTasks.jsx';
+//import PendingTasks from './components/PendingTasks.jsx';
+import NewTask from './components/NewTask';
 function App() {
   return (
     <div className="App">
-    <Header title="Tareas Pendientes" uname="David S. Betanzos" mail="dbetanzos@glwinba.com"/>
-    <PendingTasks/>
+    <Header title="Nueva Tarea" uname="David S. Betanzos" mail="dbetanzos@glwinba.com"/>
+    <NewTask employees={["Alan Fernández", "Arantxa Herrera", "Esteban Avelar", "Cynthia Fonseca"]}/>
     </div>
   );
 }
--- src/components/NewTask.jsx
+++ src/components/NewTask.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import './NewTask.css';
 
 class NewTask extends React.Component {
     render() {
+        const employees_list=this.props.employees.map((employee)=>{
+            return <option>{employee}</option>
+        })
         return <div className="NewTask">
-
+            <form className='nt_form'>
+                <div className="form_field">
+                    <h3>Titulo:</h3>
+                        <input className="input" type="text" name='title'/>
+                </div>
+                <div className="form_field">
+                    <h3>Asignar:</h3>
+                        <select className="input">
+                            {employees_list}
+                        </select>
+                </div>
+                <div className="form_field">
+                    <h3>Descripción:</h3>
+                        <textarea className="input">
+                        </textarea>
+                </div>
+                <div className="form_field">
+                    <button className="submit" type="submit">Guardar</button>
+                </div>
+            </form>
         </div>
     }
 }
